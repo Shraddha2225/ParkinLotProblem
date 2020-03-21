568815dc7cd6322643030594654f2c7755a7e513
Shraddha2225 <shraddhat22m@gmail.com>
2020-03-21 07:59:39 +0530

    ADDED:Fuctionality for Unparked car

--- src/main/java/com/bridgelab/parking/ParkingLot.java
+++ src/main/java/com/bridgelab/parking/ParkingLot.java
@@ -2,7 +2,18 @@ package com.bridgelab.parking;
 
 public class ParkingLot {
 
-    public boolean getPark(Object vehicle) {
+    private Object Car;
+
+    public boolean getPark(Object Car) {
+        this.Car = Car;
         return true;
+    }
+
+    public boolean getUnParked(Object Car) {
+        if (this.Car.equals(Car)) {
+            this.Car = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelab/parking/ParkingLotTest.java
+++ src/test/java/com/bridgelab/parking/ParkingLotTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class ParkingLotTest {
     private ParkingLot parkingLot;
+
     @Before
     public void setUp() {
         parkingLot = new ParkingLot();
@@ -16,5 +17,13 @@ public class ParkingLotTest {
         System.out.println("WELCOME TO PARKING LOT PROBLEM");
         boolean park = parkingLot.getPark(new Object());
         Assert.assertTrue(park);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        Object Car=new Object();
+        parkingLot.getPark(Car);
+        boolean unParked = parkingLot.getUnParked(Car);
+        Assert.assertTrue(unParked);
     }
 }
